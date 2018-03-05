f5cc30293609390d4a9cb1464875beece6240673
Liam Mitchell <git@liam.geek.nz>
2018-03-05 23:40:10 +0100

    Transpile builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,20 @@
-export default {
-  input: './modules/index.js',
-  output: [
-    {
-      file: 'dist/react-io.js',
-      format: 'cjs',
+import babel from 'rollup-plugin-babel'
+
+export default ['cjs', 'es', 'es2015'].map((format) => {
+  return {
+    input: './modules/index.js',
+    output: {
+      file: `dist/react-io.${format}.js`,
+      format: format === 'cjs' ? 'cjs' : 'es',
     },
-    {
-      file: 'dist/react-io.es.js',
-      format: 'es',
-    },
-  ],
-  // Treat absolute imports as external.
-  external: (id) => /^\w/.test(id),
-}
+    // Treat absolute imports as external.
+    external: (id) => /^\w/.test(id),
+    plugins: [
+      format !== 'es2015' &&
+        babel({
+          presets: [['es2015', {loose: true, modules: false}], 'react'],
+          plugins: ['external-helpers'],
+        }),
+    ].filter(Boolean),
+  }
+})
